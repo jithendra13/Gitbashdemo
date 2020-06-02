d0b7994a75b8c0bb41aa1061055176dd357ad755
Jithendra S <Jithendra.S@noida.jkt.in>
2020-06-02 08:56:52 +0530

    adding new project file

---
+++ src/main/java/java/Samplejava.java
@@ -0,0 +1,19 @@
+package java;
+
+public class Samplejava {
+
+	public void containsOrNot(String value)
+	{
+		if(value.contains("Automation"))
+		{
+			System.out.println("we are software testers");
+		}
+		else
+		{
+			System.out.println("we are manual testers");
+		
+		}
+	}
+	
+
+}
